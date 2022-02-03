cd6b60a0d3aebb5e8f353576eedc7ba6177c3f9d
YuraLen <yurii260345@gmail.com>
2022-02-03 13:28:39 +0200

    undate

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,32 +1,32 @@
 // import { parse } from 'node-html-parser';
 // const gsap = require('gsap');
 // import { gsap } from 'gsap'
-let cx, cy, mouseX, mouseY, posX, posY, clientX, clientY, dx, dy, tiltx, tilty, request, radius, degree
+// let cx, cy, mouseX, mouseY, posX, posY, clientX, clientY, dx, dy, tiltx, tilty, request, radius, degree
 
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
 
-  const body = document.querySelector("body")
-    cx = window.innerWidth / 2
-    cy = window.innerHeight / 2
+//   const body = document.querySelector("body")
+//     cx = window.innerWidth / 2
+//     cy = window.innerHeight / 2
 
-  body.addEventListener("mousemove", (e) => {
-    clientX = e.pageX
-    clientY = e.pageY
+//   body.addEventListener("mousemove", (e) => {
+//     clientX = e.pageX
+//     clientY = e.pageY
 
-    console.log(e.pageX + "/" + e.pageY);
+//     // console.log(e.pageX + "/" + e.pageY);
 
-    request = requestAnimationFrame(updateMe)
+//     request = requestAnimationFrame(updateMe)
 
-  })
+//   })
 
-  function updateMe() {
-    dx = clientX - cx
-    dy = clientY - cy
-    tiltx = dy / cy
-    tilty = dx / cx
-    radius = Math.sqrt(Math.pow(tiltx, w) + Math.pow(tilty, 2))
-    degree = radius * 12
-    gsap.to(".content", 1, {transform: `rotate3d( ${tiltx}, ${tilty}, 0, ${degree}deg)`})
-  }
+//   function updateMe() {
+//     dx = clientX - cx
+//     dy = clientY - cy
+//     tiltx = dy / cy
+//     tilty = dx / cx
+//     radius = Math.sqrt(Math.pow(tiltx, w) + Math.pow(tilty, 2))
+//     degree = radius * 12
+//     gsap.to(".content", 1, {transform: `rotate3d( ${tiltx}, ${tilty}, 0, ${degree}deg)`})
+//   }
 
-})
+// })
