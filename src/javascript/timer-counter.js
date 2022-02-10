f45143bd979b134633cdd697072be5b9b50e2187
YuraLen <yurii260345@gmail.com>
2022-02-10 18:36:59 +0200

    update

--- src/javascript/timer-counter.js
+++ src/javascript/timer-counter.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const birthday = new Date("Jun 1 2022 00:00:00");
+  const birthday = new Date("Jun 01 2022 00:00:00");
 
-  const mounthsVal = document.querySelector(`.time-count_mounths .time-count_value`)
+  const weeksVal = document.querySelector(`.time-count_weeks .time-count_value`)
   const daysVal = document.querySelector(`.time-count_days .time-count_value`)
   const hoursVal = document.querySelector(`.time-count_hours .time-count_value`)
   const minutesVal = document.querySelector(`.time-count_minutes .time-count_value`)
   const secondsVal = document.querySelector(`.time-count_seconds .time-count_value`)
 
-  const mounthsText = document.querySelector(`.time-count_mounths .time-count_text`)
+  const weeksText = document.querySelector(`.time-count_weeks .time-count_text`)
   const daysText = document.querySelector(`.time-count_days .time-count_text`)
   const hoursText = document.querySelector(`.time-count_hours .time-count_text`)
   const minutesText = document.querySelector(`.time-count_minutes .time-count_text`)
@@ -22,21 +22,50 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const timeCount = () => {
     let now = new Date()
-
     let leftUntil = birthday - now
 
-    let mounths = Math.floor(leftUntil / 1000 / 60 / 60 / 24 / 7)
+    let weeks = Math.floor(leftUntil / 1000 / 60 / 60 / 24 / 7)
     let days = Math.floor(leftUntil / 1000 / 60 / 60 / 24) % 7
     let hours = Math.floor(leftUntil / 1000 / 60 / 60) % 24
     let minutes = Math.floor(leftUntil / 1000 / 60) % 60
     let seconds = Math.floor(leftUntil / 1000) % 60
 
-    mounthsVal.textContent = mounths
-    daysVal.textContent = days
-    hoursVal.textContent = hours
-    minutesVal.textContent = minutes
-    secondsVal.textContent = seconds
+    // weeks
+    if(weeks <= 9){
+      weeksVal.innerText = "0" + weeks
+    } else {
+      weeksVal.innerText = weeks
+    }
+    
+    // days 
+    if(days <= 9){
+      daysVal.innerText = "0" + days
+    } else {
+      daysVal.innerText = days
+    }
 
+    // hours
+    if(hours <= 9){
+      hoursVal.innerText = "0" + hours
+    } else {
+      hoursVal.innerText = hours
+    }
+
+    // minutes
+    if(minutes <= 9){
+      minutesVal.innerText = "0" + minutes
+    } else {
+      minutesVal.innerText = minutes
+    }
+    
+    // seconds
+    if(seconds <= 9){
+      secondsVal.innerText = "0" + seconds
+    } else {
+      secondsVal.innerText = seconds
+    }
+
+    weeksText. textContent = declOfNum(days, ['week', 'weeks', 'weeks']);
     daysText.textContent = declOfNum(days, ['day', 'days', 'days']);
     hoursText.textContent = declOfNum(hours, ['hour', 'hours', 'hours']);
     minutesText.textContent = declOfNum(minutes, ['minute', 'minutes', 'minutes']);
